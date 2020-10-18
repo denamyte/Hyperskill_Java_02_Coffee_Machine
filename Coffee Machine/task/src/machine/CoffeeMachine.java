8bf01d7cc1e63a2dfbca5349deafaf877edf8dd1
denamyte <denamyte@gmail.com>
2020-10-19 02:20:24 +0300

    stage 2: finished

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,19 +1,33 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
-    static String[] texts = {
-            "Starting to make a coffee",
-            "Grinding coffee beans",
-            "Boiling water",
-            "Mixing boiled water with crushed coffee beans",
-            "Pouring coffee into the cup",
-            "Pouring some milk into the cup",
-            "Coffee is ready!"
-    };
+    static final String
+            prompt = "Write how many cups of coffee you will need: ",
+            caption = "For %d cups of coffee you will need:%n",
+            water = "%d ml of water%n",
+            milk = "%d ml of milk%n",
+            beans = "%d g of coffee beans%n";
 
     public static void main(String[] args) {
-        for (String text : texts) {
-            System.out.println(text);
-        }
+        System.out.println(prompt);
+        int cups = Integer.parseInt(new Scanner(System.in).nextLine());
+        System.out.printf(caption, cups);
+        System.out.printf(water, calcWater(cups));
+        System.out.printf(milk, calcMilk(cups));
+        System.out.printf(beans, calcBeans(cups));
+    }
+
+    static int calcWater(int cups) {
+        return cups * 200;
+    }
+
+    static int calcMilk(int cups) {
+        return cups * 50;
+    }
+
+    static int calcBeans(int cups) {
+        return cups * 15;
     }
 }
