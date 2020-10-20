a7c0e8d5a0665e08f2bd3c1f83fcd7fb1c704734
denamyte <denamyte@gmail.com>
2020-10-20 04:12:18 +0300

    2020-10-20: stage 3/6 finished

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,33 +1,53 @@
 package machine;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class CoffeeMachine {
-    static final String
-            prompt = "Write how many cups of coffee you will need: ",
-            caption = "For %d cups of coffee you will need:%n",
-            water = "%d ml of water%n",
-            milk = "%d ml of milk%n",
-            beans = "%d g of coffee beans%n";
+
+    static final int CUPS_INDEX = 3;
+    static final String[] QUERIES = {
+            "Write how many ml of water the coffee machine has: ",
+            "Write how many ml of milk the coffee machine has: ",
+            "Write how many grams of coffee beans the coffee machine has: ",
+            "Write how many cups of coffee you will need: "
+    };
+    static final String[] MESSAGES = {
+            "No, I can make only %d cup(s) of coffee",
+            "Yes, I can make that amount of coffee",
+            "Yes, I can make that amount of coffee (and even %d more than that)"
+    };
+    static final int[] PER_CUP = {200, 50, 15};
+    static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(prompt);
-        int cups = Integer.parseInt(new Scanner(System.in).nextLine());
-        System.out.printf(caption, cups);
-        System.out.printf(water, calcWater(cups));
-        System.out.printf(milk, calcMilk(cups));
-        System.out.printf(beans, calcBeans(cups));
+        int[] data = obtainData();
+        int maxCups = defineMaxCups(data);
+        String result = defineResult(maxCups, data[CUPS_INDEX]);
+        System.out.println(result);
     }
 
-    static int calcWater(int cups) {
-        return cups * 200;
+    static int[] obtainData() {
+        return IntStream.range(0, 4).map(CoffeeMachine::requestAmount).toArray();
     }
 
-    static int calcMilk(int cups) {
-        return cups * 50;
+    static int requestAmount(int index) {
+        System.out.println(QUERIES[index]);
+        return scanner.nextInt();
     }
 
-    static int calcBeans(int cups) {
-        return cups * 15;
+    static int defineMaxCups(int[] data) {
+        return IntStream.range(0, 3)
+                .map(i -> data[i] / PER_CUP[i])
+                .min()
+                .orElseThrow();
+    }
+
+    static String defineResult(int maxCups, int orderedCups) {
+        switch (Integer.compare(maxCups, orderedCups)) {
+            case -1: return String.format(MESSAGES[0], maxCups);                //  < 0:  not enough
+            case  0: return MESSAGES[1];                                        // == 0: exact amount
+            default: return String.format(MESSAGES[2], maxCups - orderedCups);  //  > 0:  surplus
+        }
     }
 }
