4d96b0a39b66cc0edab91854cae7f0ad8171eff6
denamyte <denamyte@gmail.com>
2020-10-23 02:38:33 +0300

    2020-10-23: stage 4/6 finished

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,53 +1,98 @@
 package machine;
 
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class CoffeeMachine {
 
-    static final int CUPS_INDEX = 3;
-    static final String[] QUERIES = {
-            "Write how many ml of water the coffee machine has: ",
-            "Write how many ml of milk the coffee machine has: ",
-            "Write how many grams of coffee beans the coffee machine has: ",
-            "Write how many cups of coffee you will need: "
+    static Amount[] beverageAmounts = {
+            new Amount(-250,    0, -16, -1, 4),  // espresso
+            new Amount(-350,  -75, -20, -1, 7),  // latte
+            new Amount(-200, -100, -12, -1, 6)   // cappuccino
     };
-    static final String[] MESSAGES = {
-            "No, I can make only %d cup(s) of coffee",
-            "Yes, I can make that amount of coffee",
-            "Yes, I can make that amount of coffee (and even %d more than that)"
-    };
-    static final int[] PER_CUP = {200, 50, 15};
-    static final Scanner scanner = new Scanner(System.in);
+
+    Amount storeAmount = new Amount(400, 540, 120, 9, 550);
+    Scanner scanner = new Scanner(System.in);
+
+    public void turnOn() {
+        showAmounts();
+        switch (acceptAction()) {
+            case "buy":  buy();
+                break;
+            case "fill": fill();
+                break;
+            case "take": take();
+                break;
+        }
+        System.out.println();
+        showAmounts();
+    }
+
+    private void showAmounts() {
+        System.out.printf(
+                "The coffee machine has:%n"
+                        + "%d of water%n"
+                        + "%d of milk%n"
+                        + "%d of coffee beans%n"
+                        + "%d of disposable cups%n"
+                        + "%d of money%n",
+                storeAmount.water, storeAmount.milk, storeAmount.beans,
+                storeAmount.cups, storeAmount.money);
+    }
+
+    private String acceptAction() {
+        System.out.println("\nWrite action (buy, fill, take): ");
+        return scanner.nextLine();
+    }
+
+    private void buy() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+        Amount amount = beverageAmounts[scanner.nextInt() - 1];
+        storeAmount.plus(amount);
+    }
+
+    private void fill() {
+        Amount fill = new Amount();
+        System.out.println("Write how many ml of water do you want to add: ");
+        fill.water = scanner.nextInt();
+        System.out.println("Write how many ml of milk do you want to add: ");
+        fill.milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans do you want to add: ");
+        fill.beans = scanner.nextInt();
+        System.out.println("Write how many disposable cups of coffee do you want to add: ");
+        fill.cups = scanner.nextInt();
+        storeAmount.plus(fill);
+    }
+
+    private void take() {
+        System.out.printf("I gave you %d%n", storeAmount.money);
+        storeAmount.money = 0;
+    }
+
+    static class Amount {
+        int water, milk, beans, cups, money;
+
+        Amount() { }
+
+        Amount(int water, int milk, int beans, int cups, int money) {
+            this.water = water;
+            this.milk = milk;
+            this.beans = beans;
+            this.cups = cups;
+            this.money = money;
+        }
+
+        /** Sum up this amount and another one. */
+        void plus(Amount other) {
+            water += other.water;
+            milk += other.milk;
+            beans += other.beans;
+            cups += other.cups;
+            money += other.money;
+        }
+
+    }
 
     public static void main(String[] args) {
-        int[] data = obtainData();
-        int maxCups = defineMaxCups(data);
-        String result = defineResult(maxCups, data[CUPS_INDEX]);
-        System.out.println(result);
-    }
-
-    static int[] obtainData() {
-        return IntStream.range(0, 4).map(CoffeeMachine::requestAmount).toArray();
-    }
-
-    static int requestAmount(int index) {
-        System.out.println(QUERIES[index]);
-        return scanner.nextInt();
-    }
-
-    static int defineMaxCups(int[] data) {
-        return IntStream.range(0, 3)
-                .map(i -> data[i] / PER_CUP[i])
-                .min()
-                .orElseThrow();
-    }
-
-    static String defineResult(int maxCups, int orderedCups) {
-        switch (Integer.compare(maxCups, orderedCups)) {
-            case -1: return String.format(MESSAGES[0], maxCups);                //  < 0:  not enough
-            case  0: return MESSAGES[1];                                        // == 0: exact amount
-            default: return String.format(MESSAGES[2], maxCups - orderedCups);  //  > 0:  surplus
-        }
+        new CoffeeMachine().turnOn();
     }
 }
---
+++ Coffee Machine/task/src/previous/CoffeeMachine3.java
@@ -0,0 +1,53 @@
+package previous;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class CoffeeMachine3 {
+
+    static final int CUPS_INDEX = 3;
+    static final String[] QUERIES = {
+            "Write how many ml of water the coffee machine has: ",
+            "Write how many ml of milk the coffee machine has: ",
+            "Write how many grams of coffee beans the coffee machine has: ",
+            "Write how many cups of coffee you will need: "
+    };
+    static final String[] MESSAGES = {
+            "No, I can make only %d cup(s) of coffee",
+            "Yes, I can make that amount of coffee",
+            "Yes, I can make that amount of coffee (and even %d more than that)"
+    };
+    static final int[] PER_CUP = {200, 50, 15};
+    static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] data = obtainData();
+        int maxCups = defineMaxCups(data);
+        String result = defineResult(maxCups, data[CUPS_INDEX]);
+        System.out.println(result);
+    }
+
+    static int[] obtainData() {
+        return IntStream.range(0, 4).map(CoffeeMachine3::requestAmount).toArray();
+    }
+
+    static int requestAmount(int index) {
+        System.out.println(QUERIES[index]);
+        return scanner.nextInt();
+    }
+
+    static int defineMaxCups(int[] data) {
+        return IntStream.range(0, 3)
+                .map(i -> data[i] / PER_CUP[i])
+                .min()
+                .orElseThrow();
+    }
+
+    static String defineResult(int maxCups, int orderedCups) {
+        switch (Integer.compare(maxCups, orderedCups)) {
+            case -1: return String.format(MESSAGES[0], maxCups);                //  < 0:  not enough
+            case  0: return MESSAGES[1];                                        // == 0: exact amount
+            default: return String.format(MESSAGES[2], maxCups - orderedCups);  //  > 0:  surplus
+        }
+    }
+}
