5547b8e4f621767c14728a2ccf4fea6d4d337153
denamyte <denamyte@gmail.com>
2020-10-28 01:40:07 +0200

    2020-10-28: 2 problems

---
+++ Problems/Project constants and methods/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stream.of(ConstantsAndUtilities.A_CONSTANT_TTT,
+                  ConstantsAndUtilities.B_CONSTANT_QQQ,
+                  ConstantsAndUtilities.getMagicString(),
+                  ConstantsAndUtilities.convertStringToAnotherString("aa"))
+                .forEach(System.out::println);
+    }
+
+}
+
+// Don't change this class
+class ConstantsAndUtilities {
+
+    public static final String A_CONSTANT_TTT = "1234";
+
+    public static final String B_CONSTANT_QQQ = "7890";
+
+    public static String getMagicString() {
+        return A_CONSTANT_TTT + B_CONSTANT_QQQ;
+    }
+
+    public static String convertStringToAnotherString(String s) {
+        return A_CONSTANT_TTT + s;
+    }
+}
